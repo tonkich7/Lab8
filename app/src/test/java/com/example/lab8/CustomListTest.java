ae1f214f885e47b489830c46fb23bb659f1019ce
tonkich7 <100548563+tonkich7@users.noreply.github.com>
2023-03-07 17:41:09 -0700

    commiting tests
    commiting tests

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cityList = new ArrayList<>();
 
+    @Test
+    public void testAddCity() {
+        CustomList list = new CustomList(null, cityList);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+    }
 
 }
